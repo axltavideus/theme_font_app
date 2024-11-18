1d677e65ac219843c240bfbe168344e7b984579e
axltavideus <yeetna01@gmail.com>
2024-11-18 14:41:14 +0700

    ilangin text theme

--- lib/theme_provider.dart
+++ lib/theme_provider.dart
@@ -6,42 +6,24 @@ class ThemeProvider with ChangeNotifier {
 
   static final lightTheme = ThemeData(
     colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.blue).copyWith(
-      surface: Colors.white,  // Set the surface to white for light theme
+      surface: Colors.white,
     ),
     fontFamily: 'Roboto',
     scaffoldBackgroundColor: Colors.white,
-    textTheme: TextTheme(
-      bodyLarge: TextStyle(color: Colors.black),  // Set default text color for light theme
-      bodyMedium: TextStyle(color: Colors.black),
-      headlineLarge: TextStyle(color: Colors.black),  // Adjust headline as well
-      headlineMedium: TextStyle(color: Colors.black),
-    ),
   );
 
   static final greenTheme = ThemeData(
     colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.green).copyWith(
-      surface: Colors.green[50],  // Set a light green surface color for green theme
+      surface: Colors.green[50],
     ),
     scaffoldBackgroundColor: Colors.green[50],
-    textTheme: TextTheme(
-      bodyLarge: TextStyle(color: Colors.black),  // Text color for green theme
-      bodyMedium: TextStyle(color: Colors.black),
-      headlineLarge: TextStyle(color: Colors.black),
-      headlineMedium: TextStyle(color: Colors.black),
-    ),
   );
 
   static final redTheme = ThemeData(
     colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.red).copyWith(
-      surface: Colors.red[50],  // Set a light red surface color for red theme
+      surface: Colors.red[50],
     ),
     scaffoldBackgroundColor: Colors.red[50],
-    textTheme: TextTheme(
-      bodyLarge: TextStyle(color: Colors.black),  // Text color for red theme
-      bodyMedium: TextStyle(color: Colors.black),
-      headlineLarge: TextStyle(color: Colors.black),
-      headlineMedium: TextStyle(color: Colors.black),
-    ),
   );
 
   ThemeData get currentTheme => _currentTheme;
